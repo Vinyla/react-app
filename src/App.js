185b53c3e15c63c31a13cc56ddeb04f6b0672b46
Vinyla <irma.rustemovic@gmail.com>
2021-04-28 14:23:23 +0200

    navigator component with navigation links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
 
 //Create a react component
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <AppHeader />
       <p>Naslov</p>
       <p>Text</p>
       <AppFooter />
-    </div>
+    </BrowserRouter>
   );
 };
 

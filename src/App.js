9b4e99a825cb099d0f018ca2ef19983828332262
Vinyla <irma.rustemovic@gmail.com>
2021-04-28 13:24:02 +0200

    basic header and footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-const labelTxt = 'First name:';
-const style = {
-  backgroundColor: 'blue',
-  color: 'white'
-};
+import AppHeader from './components/AppHeader';
+import AppFooter from './components/AppFooter';
 
 //Create a react component
 const App = () => {
   return (
     <div>
-      <div>
-        <label className='label' htmlFor='name'>
-          {labelTxt}
-        </label>
-        <input id='name' />
-        <button style={style}>Submit</button>
-      </div>
+      <AppHeader />
+      <p>Naslov</p>
+      <p>Text</p>
+      <AppFooter />
     </div>
   );
 };

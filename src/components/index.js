fa4a4ae5b471778af1dd5a2dcf85b51eac0bb790
Vinyla <irma.rustemovic@gmail.com>
2021-04-29 11:10:26 +0200

    added course component with edit error

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import AppFooter from './AppFooter';
 import AppHeader from './AppHeader';
 import Home from './Home';
 import Courses from './Courses';
+import Course from './Course';
 import Students from './Students';
 
-export { AppFooter, AppHeader, Home, Courses, Students };
+export { AppFooter, AppHeader, Home, Courses, Course, Students };
